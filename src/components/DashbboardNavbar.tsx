76e62b9eb29dbcb443cfa99947988f85ffdf1ffb
bertin <iradukundabertin082@gmail.com>
2024-10-06 13:50:23 +0200

    modify navbar

--- src/components/DashbboardNavbar.tsx
+++ src/components/DashbboardNavbar.tsx
@@ -5,20 +5,27 @@ import { Blocks } from "lucide-react";
 import { useSidebarState } from "@/hooks/useSidebarState";
 import { useAuthStore } from "@/store/useAuthStore";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const DashboardNavbar = () => {
-   const { isCollapsed, mobileWidth, isClient, toggleSidebar } =
-      useSidebarState();
-   const { user } = useAuthStore()
+   const { isCollapsed, mobileWidth, isClient } = useSidebarState();
+   const { user } = useAuthStore();
+   const pathname = usePathname(); // Get the current path
 
    if (!isClient) {
       return null;
    }
 
+   // Check if the user is in the dashboard or admin panel
+   const isDashboard = pathname.includes("/dashboard");
+   const buttonText = isDashboard ? "View Unrated Projects" : "View All Projects";
+   const buttonLink = isDashboard ? "/dashboard" : "/admin/projects";
+
    return (
       <main
-         className={`flex items-center justify-between shadow py-4  sticky top-0 bg-white z-30 transition-al duration-300 px-8 ${mobileWidth ? "hidden" : isCollapsed ? "" : ""
-            }`}
+         className={`flex items-center justify-between shadow py-4 sticky top-0 bg-white z-30 transition-all duration-300 px-8 ${
+            mobileWidth ? "hidden" : isCollapsed ? "" : ""
+         }`}
       >
          <section className="flex flex-col items-start">
             <h3 className="text-lg font-semibold">Welcome 👋,</h3>
@@ -26,9 +33,9 @@ const DashboardNavbar = () => {
          </section>
 
          <section className="flex gap-4 items-center justify-center">
-            <Link href={'/admin/projects'} className="flex bg-main rounded gap-2 items-center p-2 text-white w-auto px-4">
+            <Link href={buttonLink} className="flex bg-main rounded gap-2 items-center p-2 text-white w-auto px-4">
                <Blocks className="w-4 h-4" />
-               <small className="text-xs sm:text-sm">View All Projects</small>
+               <small className="text-xs sm:text-sm">{buttonText}</small>
             </Link>
          </section>
       </main>
